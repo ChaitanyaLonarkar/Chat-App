6f97e1dc63d20e1e72bb765a6fd0649d28459575
Chaitanyalonarkar <chaitanyalonarkar@gmail.com>
2024-01-09 19:53:32 +0530

    made seversl pages for chaat app

--- mychatapp/url.py
+++ mychatapp/url.py
@@ -5,9 +5,14 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', include('mychatapp.urls')),
-    path('', views.index),
+    path('', views.index,name='index'),
     path('login/',views.login_page,name='login_page'),
     path('register/',views.register_page,name='register_page'),
-    
+    path('home/',views.home,name='home'),
+    path('add_friend/',views.add_friend,name='add_friend'),
+    path('friend_request/',views.friend_request,name='friend_request'),
+    path('setting/',views.setting,name='setting'),
+    path('notifications/',views.notifications,name='notifications'),
+    path('home/chat/',views.chat,name='chat'),
     
 ]
--- mychatapp/views.py
+++ mychatapp/views.py
@@ -9,3 +9,21 @@ def login_page(request):
 
 def register_page(request):
     return render(request,'register.html')
+
+def home(request):
+    return render(request,'home.html')
+
+def notifications(request):
+    return render(request,'notifications.html')
+
+def add_friend(request):
+    return render(request,'add_friend.html')
+
+def friend_request(request):
+    return render(request,'friend_request.html')
+
+def setting(request):
+    return render(request,'update.html')
+
+def chat(request):
+    return render(request,'chat.html')
